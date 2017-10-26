8da12bc0cba4cc65012d0ea2972381ae56ea37be
Jorge Amorós <j1amoros944@gmail.com>
2017-10-26 11:16:40 +0200

    Update ClientExtension.java

--- src/pruebas/ClientExtension.java
+++ src/pruebas/ClientExtension.java
@@ -7,18 +7,18 @@ public class ClientExtension implements Client{
 
 	
 	public ClientExtension() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	@Override
 	public void receiveMessage(Message message) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public boolean hasEmail() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 	
